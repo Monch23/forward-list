3af0faf5e03bf81c38b07ec17f6d9f777aafcebc
Monch23 <87530575+Monch23@users.noreply.github.com>
2024-10-08 18:06:12 +0400

    Update implementation.c

--- implementation.c
+++ implementation.c
@@ -4,7 +4,31 @@
 enum {
 	SUCCESS,
 	INV_PTR,
+	SAM_DATA,
 } errors;
+
+// destructor
+
+int destroy_list(List *this) {
+	if (this == NULL) {
+		return INV_PTR;
+	}
+	if (this->head == NULL) {
+		return SUCCESS;
+	}
+
+	Node *tmp = this->head->next;
+	
+	while (tmp) {
+		free(this->head);
+		this->head = tmp;
+		tmp = tmp->next;
+	}
+
+	return SUCCESS;
+}
+
+// constructors
 
 int list_init(List *this) {
 	if (this == NULL) {
@@ -15,18 +39,101 @@ int list_init(List *this) {
 	return SUCCESS;
 }
 
-int list_init_size(List *this, size_t size) {
+int list_init_fill(List *this, size_t size, int val) {
 	if (this == NULL) {
 		return INV_PTR;
 	}
 
-	this->head = (Node*)calloc(1, sizeof(Node));
+	this->head = (Node*)malloc(sizeof(Node));
+	Node *tmp = this->head;
 	
-	for (int i = 1; i < size; ++i) {
-		this->head->next =
+	while (--size) {
+		tmp->next = (Node*)malloc(sizeof(Node));
+		tmp->next->value = val;
+		tmp = tmp->next;
+	}
+
+	tmp->next = NULL;
+	
+	return SUCCESS;
+}
+
+int list_copy(List *this, const List *other) {
+	if (this == NULL || other == NULL) {
+		return INV_PTR;
+	}
+	if (this == other) {
+		return SAM_DATA;
+	}
+
+	this->head = (Node*)malloc(sizeof(Node));
+	Node *tmp = this->head;
+	Node *tmp2 = other->head;
+	
+	while (tmp2) {
+		tmp->next = (Node*)malloc(sizeof(Node));
+		tmp->next->value = tmp2->next->value;
+		tmp = tmp->next;
+		tmp2 = tmp2->next;
+	}
+
+	tmp->next = NULL;
+
+	return SUCCESS;
+}
+
+int list_assign(List *this, const List *other) {
+	if (this == NULL || other == NULL) {
+		return INV_PTR;
+	}
+	if (this == other) {
+		return SAM_DATA;
+	}
+
+	if (this->head != NULL) {
+		destroy_list(this);
+	}
+	this->head = (Node*)malloc(sizeof(Node));
+	Node *tmp = this->head;
+	Node *tmp2 = other->head;
+
+	while (tmp2) {
+		tmp->next = (Node*)malloc(sizeof(Node));
+		tmp->next->value = tmp2->next->value;
+		tmp = tmp->next;
+		tmp2 = tmp2->next;
 	}
 
 	return SUCCESS;
 }
+
+// modifiers
+
+int list_insert(List *this, size_t pos, int val) {
+	if (this == NULL) {
+		return INV_PTR;
+	}
+
+	if (this->head == NULL) {
+		this->head = (Node*)malloc(sizeof(Node));
+		Node *tmp = this->head;
+		
+		while (pos--) {
+			tmp->next = (Node*)malloc(sizeof(Node));
+			tmp->next->value = val;
+			tmp = tmp->next;
+		}
+		
+		return SUCCESS;
+	}
+
+	Node *saveHead = this->head;
+	
+	while (count--) {
+		
+	}
+}
+
+
 
 
